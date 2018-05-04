838daaa6b9020e4f876713ac7425f14143f17b73
Marc King <marc@marc.software>
2018-05-04 00:20:06 -0400

    Initial project structure

---
+++ README.md
@@ -0,0 +1,5 @@
+# RuleSystem Library
+A .NET Core 2.0 library for implementing a 5th Edition rule system.
+
+**This project is currently in the very early stages of development. More details will be provided
+as the codebase becomes stable.**
---
+++ tests/RuleSystem.ConsoleTests/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RuleSystem.ConsoleTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("RuleSystem.ConsoleTests");
+
+            Console.ReadKey(true);
+        }
+    }
+}
